ea1bfcc52008621bc770453a603b24b9b50ae02f
Aivars Irmejs <aivars@gmail.com>
2016-01-25 14:03:19 +0200

    Yii fix for interfaces

--- yii_class.js
+++ yii_class.js
@@ -1,57 +1,60 @@
-var cl = jQuery('table.summaryTable tr').has('th:contains("Inheritance")').find('td a:first').text();
-var clextends = jQuery('table.summaryTable tr').has('th:contains("Inheritance")').find('td a:nth-child(2)').text();
-var desc = jQuery('div.class-description p strong').first().text();
+var cl = jQuery('table.summaryTable tr').has('th:contains("Inheritance")').find('td a:first').text().trim();
+
+if (cl != "") {
+  var clextends = jQuery('table.summaryTable tr').has('th:contains("Inheritance")').find('td a:nth-child(2)').text();
+  var desc = jQuery('div.class-description p strong').first().text();
 
 
-var is_static = false;
-if (jQuery('div.detail-header span.detail-header-tag.small:contains("static")').length) {
-  is_static = true;
+  var is_static = false;
+  if (jQuery('div.detail-header span.detail-header-tag.small:contains("static")').length) {
+    is_static = true;
+  }
+
+  WeBuilderAddClass(cl, desc, "", clextends, is_static);
+
+  jQuery('div.method-doc div.detail-header').each(function(i) {
+    var scope = jQuery(this).find('span.detail-header-tag').text().trim();
+    if (scope.indexOf("public") > -1 || scope == "") {
+      var desc = jQuery(this).next('div.doc-description').find('p').first().text().normalize_spaces();
+      
+      var funcname = jQuery(this).clone();
+      funcname.find('*').remove();
+      funcname = funcname.text().trim();
+      funcname = funcname.replace(/\(\)/, "");
+      
+      var func = jQuery(this).nextAll('table.detail-table').first().find('td.signature').text().normalize_spaces();
+      
+      var fnArgsRegex = /\(.*\)/;
+      var funcargs = func.match(fnArgsRegex);
+
+      var funcres = jQuery(this).nextAll('table.detail-table').first().find('th:contains("return")').next('td.param-type-col').first().text().normalize_spaces();
+      
+      var is_static = "0";
+      if (scope.indexOf("static") > -1) {
+        is_static = "1";
+      }
+
+      WeBuilderAddMethod(cl, funcname, funcargs[0], funcres, desc, is_static);
+    }
+  });
+
+
+  jQuery('div.property-doc div.detail-header').each(function(i) {
+    var scope = jQuery(this).find('span.detail-header-tag').text().trim();
+    if (scope.indexOf("public") > -1 || scope == "") {
+      var desc = jQuery(this).next('div.doc-description').find('p').first().text().normalize_spaces();
+      var fieldname = jQuery(this).clone();
+      fieldname.find('*').remove();
+      fieldname = fieldname.text().trim();
+      
+      var fieldtype = jQuery(this).nextAll('div.signature').first().find('span.signature-type').text().normalize_spaces();
+      
+      var is_static = "0";
+      if (scope.indexOf("static") > -1) {
+        is_static = "1";
+      }
+
+      WeBuilderAddProperty(cl, fieldname, fieldtype, desc, is_static);
+    }
+  });
 }
-
-WeBuilderAddClass(cl, desc, "", clextends, is_static);
-
-jQuery('div.method-doc div.detail-header').each(function(i) {
-  var scope = jQuery(this).find('span.detail-header-tag').text().trim();
-  if (scope.indexOf("public") > -1 || scope == "") {
-    var desc = jQuery(this).next('div.doc-description').find('p').first().text().normalize_spaces();
-    
-    var funcname = jQuery(this).clone();
-    funcname.find('*').remove();
-    funcname = funcname.text().trim();
-    funcname = funcname.replace(/\(\)/, "");
-    
-    var func = jQuery(this).nextAll('table.detail-table').first().find('td.signature').text().normalize_spaces();
-    
-    var fnArgsRegex = /\(.*\)/;
-    var funcargs = func.match(fnArgsRegex);
-
-    var funcres = jQuery(this).nextAll('table.detail-table').first().find('th:contains("return")').next('td.param-type-col').first().text().normalize_spaces();
-    
-    var is_static = "0";
-    if (scope.indexOf("static") > -1) {
-      is_static = "1";
-    }
-
-    WeBuilderAddMethod(cl, funcname, funcargs[0], funcres, desc, is_static);
-  }
-});
-
-
-jQuery('div.property-doc div.detail-header').each(function(i) {
-  var scope = jQuery(this).find('span.detail-header-tag').text().trim();
-  if (scope.indexOf("public") > -1 || scope == "") {
-    var desc = jQuery(this).next('div.doc-description').find('p').first().text().normalize_spaces();
-    var fieldname = jQuery(this).clone();
-    fieldname.find('*').remove();
-    fieldname = fieldname.text().trim();
-    
-    var fieldtype = jQuery(this).nextAll('div.signature').first().find('span.signature-type').text().normalize_spaces();
-    
-    var is_static = "0";
-    if (scope.indexOf("static") > -1) {
-      is_static = "1";
-    }
-
-    WeBuilderAddProperty(cl, fieldname, fieldtype, desc, is_static);
-  }
-});
