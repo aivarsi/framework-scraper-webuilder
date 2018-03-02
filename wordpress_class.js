52639fdb41c54069ee44f7063ce45b58f5817cc0
Aivars Irmejs <aivars@gmail.com>
2018-03-02 17:15:02 +0200

    Wordpess scraper fixed for current online documentation

--- wordpress_class.js
+++ wordpress_class.js
@@ -2,18 +2,17 @@ jQuery('section.class-methods ul li a').each(function(i) {
   WeBuilderExtract("wordpress_class_method.js", this.href.trim());
 });
 
-var cl = jQuery('article h1 a').text();
+var className = jQuery('article h1').first().text();
 var desc = jQuery('article section.summary p').text();
 
 var fnNameRegex = /^(.*?) /;
-var clparts = cl.match(fnNameRegex);
 
 var t = jQuery('div.source-code-container .code').text();
 
-var classdefreg = new RegExp("class\\s+" + clparts[1] + "\\sextends\\s([a-zA-Z0-9_]+)", "");
+var classdefreg = new RegExp("class\\s+" + className + "\\sextends\\s([a-zA-Z0-9_]+)", "");
 var classdefparts = t.match(classdefreg);
 var classextends = "";
 if (classdefparts && classdefparts.length > 1)
   classextends = classdefparts[1];
 
-WeBuilderAddClass(clparts[1], desc, "", classextends, false);
+WeBuilderAddClass(className, desc, "", classextends, false);
--- wordpress_func.js
+++ wordpress_func.js
@@ -1,11 +1,11 @@
-var func = jQuery('article h1 a').text();
+var func = jQuery('article h1').first().text();
 var desc = jQuery('article section.summary p').text();
 var ret = jQuery('article section.return p span.return-type').text();
 retbrackets = ret.match(/^\((.*)\)$/);
 if (retbrackets && retbrackets.length > 1)
   ret = retbrackets[1];
 
-var fnNameRegex = /^(.*?) /;
+var fnNameRegex = /^(.*?)[ \(]/;
 var fnArgsRegex = /\(.*\)/;
 var funcparts = func.match(fnNameRegex);
 var funcargs = func.match(fnArgsRegex);
